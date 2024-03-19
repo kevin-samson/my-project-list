8f1cc80a5feeb2bb30a37797e2e76779c26b4fb8
Kevin Samson <kevinsam2003@gmail.com>
2024-03-19 19:02:44 +0400

    Final Setup

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,6 +4,28 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const YEAR = new Date().getFullYear();
 const config: DocsThemeConfig = {
   logo: <span>Kevin Samson</span>,
+  head: (
+    <>
+      <meta property="og:title" content="Kevin's Project List" />
+      <meta
+        property="og:description"
+        content="A website that shows all the projects that I have built and also some info about me :)"
+      />
+      <meta
+        property="og:image"
+        content="https://kevin-samson.github.io/og-image.png"
+      />
+      <meta property="og:url" content="https://kevin-samson.github.io" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@Kevin_Samson_" />
+      <meta name="twitter:creator" content="@Kevin_Samson_" />
+      <meta name="twitter:title" content="Kevin's Project List" />
+      <meta
+        name="twitter:description"
+        content="A website that shows all the projects that I have built and also some info about me :)"
+      />
+    </>
+  ),
   editLink: {
     component: () => null,
   },
@@ -15,9 +37,36 @@ const config: DocsThemeConfig = {
   },
   footer: {
     text: (
-      <span>
-        {YEAR} ©{"Kevin Samson "}
-      </span>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <div>
+          <time>{YEAR}</time> © Kevin Samson.
+        </div>
+        <div
+          style={{ display: "flex", gap: "10px", textDecoration: "underline" }}
+        >
+          <a href="https://twitter.com/Kevin_Samson_" target="_blank">
+            Twitter
+          </a>
+          <a href="https://www.linkedin.com/in/kevin-samson--/" target="_blank">
+            Linkedin
+          </a>
+          <a href="https://github.com/kevin-samson" target="_blank">
+            GitHub
+          </a>
+          <a
+            href="https://portfolio-kevin-samson.vercel.app/assets/KevinSamsonResume-1fdfbab2.pdf"
+            target="_blank"
+          >
+            CV
+          </a>
+        </div>
+      </div>
     ),
   },
   sidebar: {
