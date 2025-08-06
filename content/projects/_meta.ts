d72328b9345d61cb3713fbe510ec5a635242815e
kevin-samson <kevinsam2003@gmail.com>
2025-08-06 22:51:52 +0530

    re-order projects

--- content/projects/_meta.ts
+++ content/projects/_meta.ts
@@ -2,19 +2,19 @@ import type { MetaRecord } from 'nextra'
  
 const meta: MetaRecord = {
   "index":{
-        "title": "Projects",
+        "title": "Projects ↓",
         "theme": {
             "typesetting": "article",
             "breadcrumb": false,
             "toc": false
           },
     },
+    "fuel-consumption" : "Predictive Analysis of Vehicle Fuel Consumption",
+    "blockchain": "Certificate verification using Blockchain",
     "BITS": "BITS Calendar Maker", 
     "BeReel": "BeReel",
     "higherlowerBot": "Higher-Lower Bot",
-    "blockchain": "Certificate verification using Blockchain",
     "tts" : "Text to Speech",
-    "fuel-consumption" : "Predictive Analysis of Vehicle Fuel Consumption",
 }
  
 export default meta
